999f2fae3472f63ef3c9d6c231c6e0dec865e66a
Ritabrata <ritabrata.rnc@gmail.com>
2021-07-15 20:10:41 +0530

    america guy commit

--- src/test/java/MavenJava/AppiumTest.java
+++ src/test/java/MavenJava/AppiumTest.java
@@ -20,5 +20,7 @@ public class AppiumTest {
 		System.out.println("ElementsUiAppium_from asia developer 2");
 		System.out.println("ElementsUiAppium_from asia developer 3");
 		System.out.println("ElementsUiAppium_from asia developer 4");
+		System.out.println("ElementsUiAppium_from american developer 5");
+		System.out.println("ElementsUiAppium_from amarican developer 6");
 	}
 }
